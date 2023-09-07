e90465c1df0872224df4dbca88600655d79a32b7
Joelson Carvalho <25236114+joelson-c@users.noreply.github.com>
2023-09-07 09:46:18 -0300

    Add server pipeline

--- server/main.ts
+++ server/main.ts
@@ -9,6 +9,7 @@ import ClientSocketHandler from './src/services/client-socket/ClientSocketHandle
 const socketHandler = container.resolve(SocketServer);
 const clientHandler = container.resolve(ClientSocketHandler);
 const port = +(process.env.NODE_PORT || 3000);
+
 socketHandler.initializeServer(port);
 clientHandler.setUpServerEvents(socketHandler.getServer());
 
