115937d622e457f06bdd9f1eacd56665755bcb74
Joelson Carvalho <25236114+joelson-c@users.noreply.github.com>
2023-09-17 21:13:20 -0300

    style: change commitlint to ts

--- commitlint.config.js
+++
@@ -1,3 +0,0 @@
-export default {
-    extends: ['@commitlint/config-conventional', '@commitlint/config-lerna-scopes']
-}
---
+++ commitlint.config.ts
@@ -0,0 +1,16 @@
+import type {UserConfig} from '@commitlint/types';
+import {RuleConfigSeverity} from '@commitlint/types';
+
+const Config: UserConfig = {
+    extends: ['@commitlint/config-conventional', '@commitlint/config-lerna-scopes'],
+    rules: {
+        'scope-enum': [RuleConfigSeverity.Error, 'always', [
+            'client',
+            'server',
+            'shared'
+        ]]
+    }
+}
+
+export default Config;
+
