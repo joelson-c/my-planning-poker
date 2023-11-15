f1c5c8901cd635a2f5ee56f8358f861ae5d038fc
Joelson Carvalho <25236114+joelson-c@users.noreply.github.com>
2023-11-15 16:44:19 -0300

    build(server): add dockerfile

--- server/src/factories/ServerFactory.ts
+++ server/src/factories/ServerFactory.ts
@@ -1,8 +1,12 @@
-import { UserServer } from 'my-planit-poker-shared/typings/ServerTypes';
+import { App } from 'uWebSockets.js';
 import { Server, ServerOptions } from 'socket.io';
+import { UserServer } from 'my-planit-poker-shared/typings/ServerTypes';
 
 export default class ServerFactory {
     build(options: Partial<ServerOptions>): UserServer {
-        return new Server(options);
+        const socketApp = App();
+        const socketServer = new Server(options);
+        socketServer.attachApp(socketApp);
+        return socketServer;
     }
 }
