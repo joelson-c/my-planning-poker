3a0d5c6ed8c5c3d0c38c8d83bdfa60061c1dcf09
Joelson Carvalho <25236114+joelson-c@users.noreply.github.com>
2024-01-03 15:47:29 -0300

    chore(client): add eslint-plugin-testing-library

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -8,7 +8,7 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'testing-library'],
   rules: {
     'react-refresh/only-export-components': [
       'off',
