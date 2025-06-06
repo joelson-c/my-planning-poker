d3a1001ce20726ac8268f29edb205305baeddd7e
Joelson Carvalho <25236114+joelson-c@users.noreply.github.com>
2025-06-05 22:34:31 -0300

    fix: vote server param

--- packages/vote-server/config/runtime.exs
+++ packages/vote-server/config/runtime.exs
@@ -9,15 +9,15 @@ import Config
 
 # ## Using releases
 #
-# If you use `mix release`, you need to explicitly enable the VoteServer
+# If you use `mix release`, you need to explicitly enable the server
 # by passing the PHX_SERVER=true when you start it:
 #
-#     PHX_SERVER=true bin/VoteServer start
+#     PHX_SERVER=true bin/vote-server start
 #
-# Alternatively, you can use `mix phx.gen.release` to generate a `bin/VoteServer`
+# Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
 # script that automatically sets the env var above.
 if System.get_env("PHX_SERVER") do
-  config :vote_server, VoteServerWeb.Endpoint, VoteServer: true
+  config :vote_server, VoteServerWeb.Endpoint, server: true
 end
 
 if config_env() == :prod do
